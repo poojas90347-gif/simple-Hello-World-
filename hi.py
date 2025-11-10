1baa68d875d3a9667c3fc66ae6ce901250950fe7
Pooja Sharma <poojas90347@example.com>
2025-11-10 16:40:19 +0530

    added schemas

--- hi.py
+++ hi.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from schemas import UserCreate 
 app= FastAPI()
 @app.get("/")
 def home():
@@ -11,4 +12,7 @@ def hi():
 @app.post("/hi")
 def hi_post():
     return{"message": "Hi from POST!"}
-    
+
+@app.post("/User")
+def create_user(user: UserCreate):
+    return{"User": user.name, "age":user.age}  
